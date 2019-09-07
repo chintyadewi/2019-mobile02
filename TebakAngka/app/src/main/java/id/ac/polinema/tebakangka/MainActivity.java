6d51d468a6f487e8c007e1f0d7b81bbd38b5b9b6
chintyadewi <chintyadewi437@gmail.com>
2019-09-07 23:20:15 +0700

    completing todo

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -1,7 +1,9 @@
 package id.ac.polinema.tebakangka;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,24 +15,57 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText numberInput;
+	private Button guessButton;
+	private int randomNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		numberInput=findViewById(R.id.number_input);
+		guessButton=findViewById(R.id.guess_button);
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-
+		Random random = new Random();
+		int min=1;
+		int max=100;
+		randomNumber=random.nextInt(max - min) + min;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+
+		int input=Integer.parseInt(numberInput.getText().toString());
+		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+		if(input==randomNumber){
+			alertDialogBuilder.setMessage("Tebakan anda benar!");
+			guessButton.setEnabled(false);
+		}else if(input>randomNumber){
+			alertDialogBuilder.setMessage("Tebakan anda terlalu besar!");
+		}else if(input<randomNumber){
+			alertDialogBuilder.setMessage("Tebakan anda terlalu kecil");
+		}
+
+		alertDialogBuilder.setNegativeButton("Tutup", new DialogInterface.OnClickListener(){
+			@Override
+			public void onClick(DialogInterface dialog, int which){
+			}
+		});
+
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		alertDialog.show();
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		numberInput.setText("");
+		guessButton.setEnabled(true);
+		initRandomNumber();
 	}
 }
